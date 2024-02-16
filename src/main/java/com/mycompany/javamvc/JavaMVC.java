9bf16fe4dba93bb275ba16f63de82712dde6c166
Breno <breno.csilva@hotmail.com>
2024-02-16 04:31:28 -0300

    Criando modelo

---
+++ src/main/java/com/mycompany/javamvc/JavaMVC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javamvc;
+
+/**
+ *
+ * @author breno
+ */
+public class JavaMVC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
